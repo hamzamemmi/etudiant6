b58536d4edfd40ce44976c432e671dfd8a9886e4
BeaNecib <61478352+BeaNecib@users.noreply.github.com>
2020-06-15 15:21:44 +0100

    Update enigmeh.h

--- enigmeh.h
+++ enigmeh.h
@@ -1,32 +1,8 @@
-#ifndef  ENIGME_H_INCLUDED 
-#define ENIGMLE_H_INCLUDED
 
-#include<stdio.h> 
-#include<stdlib.h>
-#include<SDL/SDL.h>
-#include<SDL/SDL_image.h> 
-#include <SDL/SDL_ttf.h>
-#include <string.h> 
-
-typedef struct enigmes_question
-{
-char reponse[4];
-char q[400];
-char rep1[3];
-char rep2[3];
-char rep3[3];
-char rep4[3];
-}enigmes_question;
-
-typedef struct enigme
-{
-enigmes_question en[30];
-int reponse;
-}enigme;
-
-int initialiser_enigmes();
-void generation(enigme *E,int val);
-void affichage(enigme *E ,int val);
-int resolution_enigmes(char reponse_choisie[4], int val,enigme *E);// 
-
-#endif  /* ENIGME_H_ */
+#ifndef ENIGME_H_INCLUDED
+#define ENIGME_H_INCLUDED
+void reponse(SDL_Surface *ecran,int d);
+void solution(SDL_Surface *ecran,int d);
+void enigme(SDL_Surface *ecran,int d);
+void game(SDL_Surface *ecran);
+#endif // ENIGME_H_INCLUDED
